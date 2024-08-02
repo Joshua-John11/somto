75856727681d3b4f9dab8713694046f2c1fdbb47
Joshua John <joshuabaridakarajohn@gmail.com>
2024-08-02 01:20:25 +0100

    Somto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation*/
+var typed = new Typed(".typing", {
+    strings:["","Web Designer", "Graphic Designer", "Life Coach", "Model"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true 
+}) 
